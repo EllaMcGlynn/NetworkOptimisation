f9654202a55e9e1b06709b9b1831b0e35d95c39b
EllaMcGlynn <ellamcglynn3@gmail.com>
2025-05-28 14:10:23 +0100

    Initial commit

---
+++ src/main/java/com/leea/networkoptimisation/NetworkOptimisationApplication.java
@@ -0,0 +1,13 @@
+package com.leea.networkoptimisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkOptimisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkOptimisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leea/networkoptimisation/NetworkOptimisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leea.networkoptimisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkOptimisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
